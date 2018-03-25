c55be03bb4cf7be5afc430800b158ccd57a4e5c3
sooryagangarajk <sooryagangaraj@gmail.com>
2018-03-25 12:30:17 +0530

    minor sentence update

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ var stream = T.stream('user');
 
 
 const newLocal = stream.on('follow', function (eventMSG) {
-    var text = 'Thank you for following @' + eventMSG.source.name + ' ';
+    var text = 'Thank you for following ' + eventMSG.source.name + '. We will try our level best to keep you up.';
     T.post('statuses/update', { status: text }, function (err, data, response) {
         console.log('Posted for '+ eventMSG);
     });
